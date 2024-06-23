f2d8eca2eca03c9aec38decd8e6c7fb6cf98c919
GroceyLot <108685271+GroceyLot@users.noreply.github.com>
2024-06-23 20:30:26 +0100

    Update lua.js

--- lua.js
+++ lua.js
@@ -1,30 +1,4 @@
-define("vs/basic-languages/luau/luau", ["require"], (require) => {
-  "use strict";
-  var moduleExports = (() => {
-    var __defProp = Object.defineProperty;
-    var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-    var __getOwnPropNames = Object.getOwnPropertyNames;
-    var __hasOwnProp = Object.prototype.hasOwnProperty;
-    var __export = (target, all) => {
-      for (var name in all)
-        __defProp(target, name, { get: all[name], enumerable: true });
-    };
-    var __copyProps = (to, from, except, desc) => {
-      if (from && typeof from === "object" || typeof from === "function") {
-        for (let key of __getOwnPropNames(from))
-          if (!__hasOwnProp.call(to, key) && key !== except)
-            __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-      }
-      return to;
-    };
-    var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
-    // src/basic-languages/lua/lua.ts
-    var lua_exports = {};
-    __export(lua_exports, {
-      conf: () => conf,
-      language: () => language
-    });
     var conf = {
       comments: {
         lineComment: "--",
@@ -190,10 +164,6 @@ define("vs/basic-languages/luau/luau", ["require"], (require) => {
         ]
       }
     };
-    return __toCommonJS(lua_exports);
-  })();
-  return moduleExports;
-});
 
 // Register the completion item provider for Lua language
 monaco.languages.registerCompletionItemProvider('lua', {
